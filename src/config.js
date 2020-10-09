cf2904d072efd9c9bd17ede72453202cbc534bb3
Madison Brown <59658823+mbcodes29@users.noreply.github.com>
2020-10-09 13:49:15 -0400

    deploying

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL
+  DATABASE_URL: process.env.DATABASE_URL
     || 'postgresql://dunder_mifflin@localhost/capstone2',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflinl@localhost/capstone2-test',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
