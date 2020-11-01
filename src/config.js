10c3014c4dafa25c07251daf034202ba3331c323
El Moctar Ebnou <ebnoumoctar@gmail.com>
2020-11-01 15:43:57 -0600

    fix

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL
-    || 'postgresql://dunder_mifflin@localhost/capstone2',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflinl@localhost/capstone2-test',
+    || 'postgresql://moctar@localhost/capstone2',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://moctar@localhost/capstone2_test',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 };
--- test/setup.js
+++ test/setup.js
@@ -6,8 +6,8 @@ process.env.JWT_EXPIRY = '3m';
 
 require('dotenv').config();
 
-process.env.TEST_DB_URL = process.env.TEST_DB_URL
-  || "postgresql://dunder_mifflin@localhost/capstone2-test";
+process.env.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL
+  || "postgresql://moctar@localhost/capstone2_test";
 
 const { expect } = require('chai');
 const supertest = require('supertest');
